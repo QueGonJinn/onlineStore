c3a174231c11c899be87f02455dd27cb910de9b0
Yury Moliakow <molialow13@gmail.com>
2022-10-17 14:35:30 +0300

    feat: burger menu implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './index.html'; 
 import './index.scss';
 
+const inputBTN = document.querySelector('.search__button');
+const input = document.querySelector('.input');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu__next');
+const closeElem = document.querySelector('.menu__next__close');
+
+console.log(inputBTN);
+console.log(input);
+
+inputBTN.addEventListener('click', function() {
+	input.classList.toggle('hidden');
+})
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
